1fd58cb8ed6e718b645dbfef02216c6b1b773226
Vinícius Nascimento <31168053+nascivini@users.noreply.github.com>
2018-05-01 23:09:16 -0300

    Add initial files via upload.

--- src/Hash/Hash.java
+++ src/Hash/Hash.java
@@ -1,98 +1,132 @@
 package Hash;
 
-//@author Vinicius Nascimento
-//@author Marco Aurélio Geremias
-//Estruturas de Dados, 2018-1. De Lucca - Universidade Federal de Santa Catarina, UFSC
-
+/*
+@author Vinicius Nascimento
+@author Marco Aurélio Geremias
+Data Structures, 2018-1. Profº De Lucca - Federal University of Santa Catarina, Brazil, UFSC
+*/
 public class Hash {
 
 	private int [][] hash;
-	private int tamanhoEspalhamento;
+	private int scatteringAreaSize;
 
-	public Hash(int tamanhoEspalhamento, int tamanhoHash){
-		this.hash = new int [tamanhoHash][2];
-		this.tamanhoEspalhamento = tamanhoEspalhamento;
-		for(int i = 0; i < tamanhoHash; i++){
+	public Hash(int scatteringAreaSize, int hashSize){
+		this.hash = new int [hashSize][2];
+		this.scatteringAreaSize = scatteringAreaSize;
+		for(int i = 0; i < hashSize; i++){
 			this.hash[i][1] = -1;
 		}
-		//seta os próximos do hash como "-1";
+		//set all the positions with an -1
 	}
 
-	public void inserir(int novoElemento){
-		int grupo = novoElemento % tamanhoEspalhamento;
-		if(grupo == -1) {
-			grupo = grupo + 1;
+	public void insert(int newElement){
+		int group = newElement % scatteringAreaSize;
+		if(group == -1) {
+			group = group + 1;
 		}
-		if(this.hash[grupo][1] == -1){
-			this.hash[grupo][0] = novoElemento;
-			this.hash[grupo][1] = grupo + tamanhoEspalhamento;
+		if(this.hash[group][1] == -1){
+			this.hash[group][0] = newElement;
+			this.hash[group][1] = group + scatteringAreaSize;
 		}
 
 		else{
-			int controle = grupo + tamanhoEspalhamento;
-				while(controle != -1){
-					grupo = grupo + tamanhoEspalhamento;
-					controle = this.hash[grupo][1];
-				}
-				//busca a próxima posição do grupo vazia
+			int control = group + scatteringAreaSize;
+			while(control != -1){
+				group = group + scatteringAreaSize;
+				control = this.hash[group][1];
+			}
+			//find the next empty position
 
-				this.hash[grupo][0] = novoElemento; 
-				// realiza a inserção
-				
-				if(grupo + tamanhoEspalhamento <= this.hash.length -1){
-					this.hash[grupo][1] = grupo + tamanhoEspalhamento;
-				}
-				else{
-					System.out.println("O hash está cheio para elementos com final " + novoElemento%tamanhoEspalhamento + ".");
-				}
+			this.hash[group][0] = newElement; 
+			// insert the element
+
+			if(group + scatteringAreaSize <= this.hash.length -1){
+				this.hash[group][1] = group + scatteringAreaSize;
+			}
+			else{
+				System.out.println("The structure is full for elements which end on " + newElement%scatteringAreaSize + ".");
+			}
 		}
 
 	}
 
-	public void excluir (int elemento) throws Exception{
-      if (this.buscar(elemento)){
-      	int controle;
-      	for(int i = elemento % tamanhoEspalhamento; i > 0; i = i - tamanhoEspalhamento){
-      		if (this.hash[i][0] == elemento){
-      			if (this.hash [i][1] != -1){
-      				controle = this.hash[i][1];
-      				for (int j = controle; j < this.hash.length; j = j + tamanhoEspalhamento){
-      					this.hash[j][0] = this.hash[controle][0];
-      					if(this.hash[j][1] != -1){
-							this.hash[j][1] = controle;
-							controle = this.hash[j][1];
-						}
-      				}
-      			}
-      		}
-      	}
-      	throw new Exception("Elemento não encontrado! Por gentileza, inseri-lo antes da exclusão.");
-   	  }
-       
-    }
-	
-
-	public boolean buscar (int elemento){
-       int grupo = elemento % tamanhoEspalhamento;
-       
-       for (int i = grupo; i < this.hash.length; i = i + this.tamanhoEspalhamento){
-       	if (this.hash[grupo][0] == elemento){
-       		return true;
-       	}
-       	else {
-       		grupo = grupo + tamanhoEspalhamento;
-       	}
-       }
-       return false;
-       
+	public void delete (int element){
+		if(this.search(element)) {
+			//verify if the element is on the scattering area
+			int group = element % scatteringAreaSize;
+			if(this.hash[group][0] == element) {
+				//verify if the element has next elements
+				if(this.hasNext(group)) {
+					for(int i = group; hasNext(i); i = i + scatteringAreaSize) {
+						this.hash[group][0] = this.hash[this.hash[group][1]][0];
+						this.hash[group][1] = group + scatteringAreaSize;
+					}
+					if(group > scatteringAreaSize) {
+						this.hash[group - scatteringAreaSize][1] = -1;
+					}
+					else {
+						this.hash[group][1] = -1;
+					}
+				}
+				this.hash[group][0] = 0;
+			}
+			else {
+				for(int i = group; hasNext(i); i = i + scatteringAreaSize) {
+					if(this.hash[i][0] == element) {
+						group = i;
+					}
+				}
+				if(this.hasNext(group)) {
+					for(int i = group; hasNext(i); i = i + scatteringAreaSize) {
+						this.hash[group][0] = this.hash[this.hash[group][1]][0];
+						this.hash[group][1] = group + scatteringAreaSize;
+					}
+					if(group > scatteringAreaSize) {
+						this.hash[group - scatteringAreaSize][1] = -1;
+					}
+					else {
+						this.hash[group][1] = -1;
+					}
+					}
+				this.hash[group][0] = 0;
+				this.hash[group][1] = -1;
+			}		
+		}
+		else {
+			System.out.println("Element " + element + " was not found.");
+		}
 	}
-	
+
+
+	public boolean search (int element){
+		int group = element % scatteringAreaSize;
+
+		for (int i = group; i < this.hash.length; i = i + this.scatteringAreaSize){
+			if (this.hash[group][0] == element){
+				return true;
+			}
+			else {
+				group = group + scatteringAreaSize;
+			}
+		}
+		return false;
+
+	}
+
+	public boolean hasNext(int position) {
+		if(this.hash[position][1] != -1) {
+			return true;
+		}
+		return false;
+	}
+
 	public void printHash() {
-		
+
 		for(int i = 0; i < this.hash.length; i ++) {
-		System.out.println("[ " + this.hash[i][0] + " ] "+ "[ " + this.hash[i][1] + " ]");
+			System.out.println("Posição: " + i + ", Dado: " + this.hash[i][0] + ", Próxima Posição:  " + this.hash[i][1]);
 		}
-		
+		System.out.println("------------------------------------------------------------------------");
+
 	}
 
 
@@ -113,4 +147,3 @@ public class Hash {
 
 
 }
-    
--- src/Hash/main.java
+++ src/Hash/main.java
@@ -1,25 +1,39 @@
 package Hash;
 
+import java.util.Scanner;
+
 public class main {
-	
+
 	public static void main(String [] args) {
+		Scanner data = new Scanner(System.in);
+		System.out.println("Bem vindo! Para criar um novo Hash, insira os dados solicitados e tecle Enter");
+		System.out.println("Tamanho do Hash: ");
+		int sizeHash = data.nextInt();
+		System.out.println("Tamanho do Espalhamento: ");
+		int sizeScattering = data.nextInt();
+		System.out.println("Você criou um hash com tamanho " + sizeHash + " e espalhamento " + sizeScattering + ".");
+		System.out.println("Serão executados 3 baterias de teste automatizado. Iniciando ...");
+		System.out.println("");
 		
-		Hash teste = new Hash(5, 15);
-		try {
-			teste.inserir(8);
-			teste.inserir(5);
-			teste.inserir(7);
-			teste.inserir(10);
-			teste.inserir(11);
-			teste.inserir(6);
-			teste.inserir(12);
+		for(int i = 0; i < 3; i++) {
+			Hash teste = new Hash(sizeScattering, sizeHash);
+			System.out.println("Executando teste "+ i + " ...");
+			teste.insert(8);
+			teste.insert(5);
+			teste.insert(7);
+			teste.insert(10);
+			teste.insert(11);
+			teste.insert(6);
+			teste.insert(12);
 			teste.printHash();
-			teste.excluir(12);
+			teste.delete(12);
+			teste.delete(6);
+			teste.delete(11);
+			teste.delete(10);
+			teste.delete(7);
+			teste.delete(5);
+			teste.delete(8);
 			teste.printHash();
-			
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
 		}
 	}
 }
